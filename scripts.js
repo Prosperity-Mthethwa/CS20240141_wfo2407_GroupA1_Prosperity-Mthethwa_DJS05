939e98070918205a18914d85d415eae1a9df54ed
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-11-08 15:02:51 +0200

    added new listeners to the store

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
 //created function to hold the state tree
-function createStore (reducer) {
+function createStore(reducer) {
     var state;
     var listeners = [];
 
-    function getState (){
+    function getState() {
         return state;
     }
 };
 
 //dispatches action to update state of the store 
-function dispatch (action) {
+function dispatch(action) {
     state = reducer(state, action); // state updator by calling reducer current state and action
     for (var i = 0; i < listeners.length; i++) {
         listeners[i]();
     };
 };
 
+function subscribe(callback) {
+    listeners.push(callback);
+
+    dispatch({}); //initializes the state by dispatching an empty action.
+
+    return { getState: getState, dispatch: dispatch, subcribe: subscribe };
+}
