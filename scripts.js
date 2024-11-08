22447d78966616be496ee9d0ebfb695e733d8b66
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-11-08 14:33:28 +0200

    added function for state tree

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//created function to hold the state tree
+function createStore (reducer) {
+    var state;
+    var listeners = [];
+
+    function getState (){
+        return state;
+    }
+};
+
